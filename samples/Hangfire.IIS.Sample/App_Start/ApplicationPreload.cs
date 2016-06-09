22967781f1d095612d159c360440df00053221e1
Sergey Odinokov <odinserj@gmail.com>
2016-06-09 17:08:38 +0300

    Initial implementation with a lot of TODOs

---
+++ samples/Hangfire.IIS.Sample/App_Start/ApplicationPreload.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Hosting;
+using Hangfire.MemoryStorage;
+
+namespace Hangfire.IIS.Sample
+{
+    public class ApplicationPreload : IProcessHostPreloadClient
+    {
+        public static IEnumerable<IDisposable> GetHangfireConfiguration()
+        {
+            GlobalConfiguration.Configuration.UseMemoryStorage();
+
+            yield return new BackgroundJobServer();
+        }
+
+        public void Preload(string[] parameters)
+        {
+            AspNetHangfireServer.Use(GetHangfireConfiguration);
+        }
+    }
+}
---
+++ src/Hangfire.IIS/AspNetHangfireServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Hangfire.Annotations;
+using Owin;
+
+namespace Hangfire.IIS
+{
+    public static class AspNetHangfireServer
+    {
+        private static readonly RegisteredObjectWrapper Instance = new RegisteredObjectWrapper();
+
+        /// <summary>
+        /// Call this in global.asax for debugging purposes
+        /// </summary>
+        public static void Use([NotNull] Func<IEnumerable<IDisposable>> configuration)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            Instance.Start(configuration);
+        }
+
+        public static IAppBuilder UseHangfire(
+            [NotNull] this IAppBuilder builder,
+            [NotNull] Func<IEnumerable<IDisposable>> configuration)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            Use(configuration);
+
+            return builder;
+        }
+    }
+}
