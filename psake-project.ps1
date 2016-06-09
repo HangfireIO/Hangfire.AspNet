1e3702896af09875b9d71eeb6f5805590d9d344d
Sergey Odinokov <odinserj@gmail.com>
2016-06-09 16:25:40 +0300

    Add build scripts

---
+++ psake-project.ps1
@@ -0,0 +1,25 @@
+Framework 4.5.1
+Properties {
+    $solution = "Hangfire.IIS.sln"
+}
+
+Include "packages\Hangfire.Build.0.1.3\tools\psake-common.ps1"
+
+Task Default -Depends Collect
+
+Task Collect -Depends Compile -Description "Copy all artifacts to the build folder." {
+    Collect-Assembly "Hangfire.IIS" "Net45"
+}
+
+Task Pack -Depends Collect -Description "Create NuGet packages and archive files." {
+    $version = Get-BuildVersion
+
+    $tag = $env:APPVEYOR_REPO_TAG_NAME
+    if ($tag -And $tag.StartsWith("v$version-")) {
+        "Using tag-based version for packages."
+        $version = $tag.Substring(1)
+    }
+    
+    Create-Archive "Hangfire.IIS-$version"
+    Create-Package "Hangfire.IIS" $version
+}
